cc82aeab6ffa602856c7a850b6d08b1c0ea32c85
sorrowfulliu <3185435464@qq.com>
2020-06-05 16:22:44 +0800

    June 5 2020

---
+++ JianZhiOffer/10.2青蛙跳台阶问题.cpp
@@ -0,0 +1,70 @@
+﻿#include "header.h"
+
+/*
+
+一只青蛙一次可以跳上1级台阶，也可以跳上2级台阶。求该青蛙跳上一个 n 级的台阶总共有多少种跳法。
+答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
+
+示例 1：
+输入：n = 2
+输出：2
+
+示例 2：
+输入：n = 7
+输出：21
+
+提示：
+0 <= n <= 100
+
+注意：本题与主站 70 题相同：https://leetcode-cn.com/problems/climbing-stairs/
+
+*/
+
+//自上而下备忘录
+class Solution1 {
+public:
+	int numWays(int n) 
+	{
+		vector<int> memo(n + 1, 0);
+		return helper(n, memo);
+	}
+	int helper(int n, vector<int> &memo)
+	{
+		if (n == 0 || n == 1) return 1;
+		if (memo[n] != 0) return memo[n];
+		return memo[n] = (helper(n - 1, memo) + helper(n - 2, memo)) % 1000000007;
+	}
+};
+
+//自底而上动态规划
+class Solution2 {
+public:
+	int numWays(int n)
+	{
+		if (n == 0 || n == 1) return 1;
+		vector<int> dp(n + 1, 0);
+		dp[0] = 1, dp[1] = 1;
+		for (int i = 2; i <= n; ++i)
+		{
+			dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
+		}
+		return dp[n];
+	}
+};
+
+//优化空间复杂度
+class Solution3 {
+public:
+	int numWays(int n)
+	{
+		if (n == 0 || n == 1) return 1;
+		int pre = 1, cur = 1;
+		for (int i = 2; i <= n; ++i)
+		{
+			int tmp = (pre + cur) % 1000000007;
+			pre = cur;
+			cur = tmp;
+		}
+		return cur;
+	}
+};
---
+++ JianZhiOffer/11.旋转数组的最小数字.cpp
@@ -0,0 +1,35 @@
+﻿#include "header.h"
+
+/*
+
+把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。
+输入一个递增排序的数组的一个旋转，输出旋转数组的最小元素。例如，数组 [3,4,5,1,2] 为 [1,2,3,4,5] 的一个旋转，该数组的最小值为1。  
+
+示例 1：
+输入：[3,4,5,1,2]
+输出：1
+
+示例 2：
+输入：[2,2,2,0,1]
+输出：0
+
+注意：本题与主站 154 题相同：https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array-ii/
+
+*/
+
+class Solution {
+public:
+	int minArray(vector<int> &numbers) 
+	{
+		int len = numbers.size();
+		if (len == 0) return 0;
+		int lo = 0, hi = len - 1;
+		while (lo < hi)
+		{
+			if (numbers[lo]<numbers[hi]) return numbers[lo];
+			int mi = lo + ((hi - lo) >> 1);
+			(numbers[mi] > numbers[hi]) ? lo = mi + 1 : (numbers[mi] < numbers[hi]) ? hi = mi : --hi;
+		}
+		return numbers[lo];
+	}
+};
