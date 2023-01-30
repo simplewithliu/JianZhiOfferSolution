7900967aeff5d0b032e286c634bfaa450ff6aff1
sorrowfulliu <3185435464@qq.com>
2023-01-30 23:17:22 +0800

    Jan 30 2023

---
+++ JianZhiOffer/II 092.翻转字符.cpp
@@ -0,0 +1,59 @@
+#include "header.h"
+
+/*
+
+剑指 Offer II 092. 翻转字符
+
+如果一个二进制字符串，是以一些 0（可能没有 0）后面跟着一些 1（也可能没有 1）的形式组成的，那么该字符串是单调递增的
+给你一个二进制字符串 s，你可以将任何 0 翻转为 1 或者将 1 翻转为 0
+返回使 s 单调递增的最小翻转次数。
+
+示例 1：
+输入：s = "00110"
+输出：1
+解释：翻转最后一位得到 00111
+
+示例 2：
+输入：s = "010110"
+输出：2
+解释：翻转得到 011111，或者是 000111
+
+示例 3：
+输入：s = "00011000"
+输出：2
+解释：翻转得到 00000000
+
+提示：
+1 <= s.length <= 10^5
+s[i] 为 '0' 或 '1'
+
+注意：本题与主站 926 题相同： https://leetcode-cn.com/problems/flip-string-to-monotone-increasing/
+
+
+*/
+
+// 备忘录
+class Solution1 {
+public:
+	int minFlipsMonoIncr(string s) {
+		int len = s.size();
+		vector<vector<int>> memo(len + 1, vector<int>(2, -1));
+		return min(helper(s, len, '0', memo), helper(s, len, '1', memo));
+	}
+
+private:
+	int helper(string &s, int idx, char c, vector<vector<int>> &memo) {
+		if (idx == 0) return 0;
+		if (memo[idx][c - '0'] != -1) return memo[idx][c - '0'];
+		if (c == '0') {
+			return memo[idx][c - '0'] = helper(s, idx - 1, '0', memo) + (s[idx - 1] == '1');
+		}
+		else {
+			return memo[idx][c - '0'] = min(helper(s, idx - 1, '0', memo),
+				helper(s, idx - 1, '1', memo)) + (s[idx - 1] == '0');
+		}
+	}
+};
+// https://leetcode.cn/problems/flip-string-to-monotone-increasing/solutions/1592230/jiang-zi-fu-chuan-fan-zhuan-dao-dan-diao-stjd 
+// @author 力扣官方题解
+
---
+++ JianZhiOffer/II 094.最少回文分割.cpp
@@ -0,0 +1,57 @@
+#include "header.h"
+
+/*
+
+剑指 Offer II 094.最少回文分割
+
+给你一个字符串 s，请你将 s 分割成一些子串，使每个子串都是回文。返回符合要求的最少分割次数 。
+
+示例 1：
+输入：s = "aab"
+输出：1
+解释：只需一次分割就可将 s 分割成 ["aa","b"] 这样两个回文子串。
+
+示例 2：
+输入：s = "a"
+输出：0
+
+示例 3：
+输入：s = "ab"
+输出：1
+
+提示：
+1 <= s.length <= 2000
+s 仅由小写英文字母组成
+
+注意：本题与主站 132 题相同： https://leetcode-cn.com/problems/palindrome-partitioning-ii/
+
+*/
+
+// 备忘录
+class Solution1 {
+public:
+	int minCut(string s) {
+		int len = s.size();
+		vector<vector<char>> isPal(len, vector<char>(len, false));
+		for (int palLen = 1; palLen <= len; ++palLen) {
+			for (int i = 0; i <= len - palLen; ++i) {
+				int j = i + palLen - 1;
+				isPal[i][j] = (s[i] == s[j] && (palLen < 3 || isPal[i + 1][j - 1]));
+			}
+		}
+		vector<int> memo(len, -1);
+		return helper(s, len - 1, isPal, memo);
+	}
+
+private:
+	int helper(string &s, int end, vector<vector<char>> &isPal, vector<int> &memo) {
+		if (memo[end] != -1) return memo[end];
+		int len = s.size();
+		if (isPal[0][end]) return 0;
+		int minNum = INT_MAX;
+		for (int i = end; i >= 0; --i) {
+			if (isPal[i][end]) minNum = min(minNum, 1 + helper(s, i - 1, isPal, memo));
+		}
+		return memo[end] = minNum;
+	}
+};
